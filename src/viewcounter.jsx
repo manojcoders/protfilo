8b32f283e5c4240c19a4033ea0f0512252034dda
manojcoders <kandulamanj28@gmail.com>
2025-09-04 14:24:19 +0530

    views change

--- src/viewcounter.jsx
+++ src/viewcounter.jsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from "react";
 const ViewCounter = () => {
   const [views, setViews] = useState(0);
 
-  useEffect(() => {
-    fetch("https://api.countapi.xyz/hit/protfilo-manoj.vercel.app/portfolio")
+ useEffect(() => {
+    fetch("https://api.countapi.xyz/hit/protfilo-manoj/portfolio")
       .then((res) => res.json())
       .then((data) => setViews(data.value))
       .catch((err) => console.error("Error fetching views:", err));
